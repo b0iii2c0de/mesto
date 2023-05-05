49d1d87d5a86f765fd8b9b3cec55f03546a16efc
c0de b0iii <boiiicode@gmail.com>
2023-05-05 23:11:19 +0300

    refactor: move const-s to dedicated file, set-up imports to index.js

---
+++ scripts/components/PopupWithForm.js
@@ -0,0 +1,6 @@
+import Popup from "./Popup.js";
+import { profileForm, selectors } from "../../utils/constants.js";
+
+export default class PopupWithForm extends Popup {
+  
+}
---
+++ utils/constants.js
@@ -0,0 +1,85 @@
+// Selectors
+const classSelectors = {
+  typeErrorModeration: "pop-up__input_type-error",
+  errorActiveModeration: "pop-up__input-error_active",
+  inputElement: ".pop-up__input",
+  saveBtnElement: ".pop-up__sub-butt",
+  saveBtnElementOff: "pop-up__sub-butt_disabled",
+};
+
+// Get the modal elements
+const profileModalWindow = document.querySelector(".profile-pop-up");
+const cardModalWindow = document.querySelector(".card-pop-up");
+
+// DEL, used in Card class ???
+const imgModalWindow = document.querySelector(".img-pop-up");
+const fullSizeImg = imgModalWindow.querySelector(".pop-up__image");
+const fullSizeImgCap = imgModalWindow.querySelector(".pop-up__caption");
+
+const profileForm = document.querySelector(".pop-up__form");
+const profileFormEdit = document.querySelector(".pop-up__form-edit"); // used in formVal fn
+const cardForm = document.querySelector(".pop-up__form_submit"); // used in formVal fn
+
+// Get profile values and input values
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+const nameInput = profileModalWindow.querySelector("#name");
+const descriptionInput = profileModalWindow.querySelector("#description");
+
+// Get card input values
+const placeNameIn = cardModalWindow.querySelector("#place-name");
+const placeImgLinkIn = cardModalWindow.querySelector("#img-link");
+
+// Get buttons
+const profileEditBtn = document.querySelector(".profile__edit-btn");
+const profileAddBtn = document.querySelector(".profile__add-btn");
+
+// Get template
+const cardsGrid = document.querySelector(".elements__grid");
+
+// An array of cards
+const primeCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+export {
+  classSelectors,
+  profileModalWindow,
+  cardModalWindow,
+  profileForm,
+  profileFormEdit,
+  cardForm,
+  profileTitle,
+  profileSubtitle,
+  nameInput,
+  descriptionInput,
+  placeNameIn,
+  placeImgLinkIn,
+  profileEditBtn,
+  profileAddBtn,
+  cardsGrid,
+  primeCards
+};
