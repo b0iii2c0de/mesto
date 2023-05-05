2a6587f43d8b2f5f9910d8b8a727e7fe556dd8d8
c0de b0iii <boiiicode@gmail.com>
2023-05-05 23:54:03 +0300

    feat: add PopupWithForm class

--- scripts/components/PopupWithForm.js
+++ scripts/components/PopupWithForm.js
@@ -1,6 +1,41 @@
 import Popup from "./Popup.js";
-import { profileForm, selectors } from "../../utils/constants.js";
+import { profileForm, classSelectors } from "../../utils/constants.js";
 
 export default class PopupWithForm extends Popup {
-  
+  constructor(popupSelector, submitForm) {
+    super(popupSelector);
+    this._submitForm = submitForm;
+    this._btnElement = this._popupSelector.querySelector(
+      classSelectors.saveBtnElement
+    );
+  }
+
+  _getInputValues() {
+    this._inputsList = this._popupSelector.querySelectorAll(
+      classSelectors.inputElement
+    );
+
+    this._formValues = {};
+    this._inputsList.forEach(
+      (input) => (this._formValues[input.name] = input.value)
+    );
+
+    return this._formValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._popupSelector
+      .querySelector(profileForm)
+      .addEventListeners("submit", (evt) => {
+        this._submitForm(evt, this._getInputValues());
+      });
+  }
+
+  close() {
+    super.close();
+    this._popupSelector.querySelector(profileForm).reset();
+    this._btnElement.classList.add(classSelectors.saveBtnElementOff);
+    this._btnElement.setAttribute("disabled", "");
+  }
 }
